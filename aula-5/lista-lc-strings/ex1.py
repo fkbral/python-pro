2b54dc24fcb6952ca96b677fc9cdc4d2e40f7a06
Felipe Cabral <felipeo.cabral@hotmail.com>
2021-04-19 22:24:02 -0300

    feat: exercícios 1 e 2 da lista LC | Strings

---
+++ aula-5/lista-lc-strings/ex1.py
@@ -0,0 +1,6 @@
+# Faça um programa que peça para o usuário digitar
+# uma palavra e imprima cada letra em uma linha.
+
+palavra = 'Bom dia a todos'
+for letra in palavra:
+    print(letra)
---
+++ aula-5/lista-lc-strings/ex2.py
@@ -0,0 +1,10 @@
+nome = "Renata"
+nome_copiado = ""
+
+for letra in nome:
+    nome_copiado += letra
+
+# for indice in range(len(nome)):
+#   nome_copiado += nome[indice]
+
+print(nome_copiado)
