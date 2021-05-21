1ce3b1a46ff26d2db084c247847d45bbb00fd7e8
Felipe Cabral <38699509+fkbral@users.noreply.github.com>
2021-05-21 18:54:59 -0300

    feat: alternativa com sintaxe mais limpa para servidor Flask

---
+++ aula-12/api-rest/server-better.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify, render_template, request
+import pandas as pd
+
+users = pd.read_csv('aula-12/api-rest/users.csv').to_dict('records')
+
+api = Flask(__name__)
+
+@api.route('/')
+def home():
+  return render_template('home.html')
+
+@api.route('/users')
+def list_users():
+  return jsonify(users)
+
+@api.route('/users', methods=['POST'])
+def store_user():
+  new_user = {
+    "id": len(users) + 1,
+    "name": request.json["name"],
+    "occupation": request.json["occupation"],
+    "avatar_url": request.json["avatar_url"]
+  }
+
+  users.append(new_user)
+
+  users_data_frame = pd.DataFrame(users)
+  users_data_frame.to_csv('aula-12/api-rest/users.csv', index=False)
+
+  return jsonify(new_user)
+
+@api.route('/users-view')
+def render_users():
+  return render_template('list_users.html', users=users)
+
+# if __name__ == '__main__':
+api.run()
